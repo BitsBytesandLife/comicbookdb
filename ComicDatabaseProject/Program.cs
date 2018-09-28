56e9c6193d3df153e2f82acc3ae6f7c0ab26e359
BK <brian_j_king@me.com>
2018-09-28 18:11:18 -0500

    First Commit

--- ComicDatabaseProject/Program.cs
+++ ComicDatabaseProject/Program.cs
@@ -7,6 +7,8 @@ namespace ComicDatabaseProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
